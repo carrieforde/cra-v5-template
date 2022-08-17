d36358b6d4240fd0349c3b02c76701e51d9e2415
Carrie Forde <carrie@carrieforde.com>
2022-08-17 10:19:55 -0700

    feat: remove Material

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App/App";
 import reportWebVitals from "./reportWebVitals";
-import { CssBaseline } from "@mui/material";
-import { ThemeProvider } from "@mui/material/styles";
-import { theme } from "mui/theme";
 import "styles/index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
